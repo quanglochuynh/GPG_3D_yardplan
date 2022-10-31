b16eb363f5d180efd0410bc2fecaa2a50b35d0b3
Loc Huynh <quanglochuynh@gmail.com>
2022-10-31 17:57:25 +0700

    .

--- yardplan_creator/sketch.js
+++ yardplan_creator/sketch.js
@@ -3,6 +3,7 @@ let depot;
 let mode="view";
 let newAreaStart=undefined;
 let newAreaEnd=undefined;
+const scaleFactor = 0.35;
 
 class Point{
   constructor(x,y){
@@ -28,46 +29,50 @@ class Area{
 
 function drawDepot(){
   push();
+  // erase()
   translate(-height/2, width/2);
   strokeWeight(2)
-  beginShape();
-  for (let j=1; j<depot.layout.shape[0].length; j++){
-    p1 = depot.layout.shape[0].seq[j];
-    vertex(p1.x,p1.y,0)
+  // fill(0)
+  for (let i=0; i<depot.ground.length;i++){
+    beginShape();
+    for (let j=1; j<depot.layout.shape[i].length; j++){
+      p1 = depot.layout.shape[i].seq[j];
+      vertex(p1.x,p1.y,0)
+    }
+    endShape(CLOSE);
   }
-  endShape(CLOSE);
-  noFill();
-  for (let i =0; i<depot.layout.shape[0].length-1; i++){
-    p1 = depot.layout.shape[0].seq[i]
-    p2 = depot.layout.shape[0].seq[i+1]
-    line(p1.x, p1.y, p2.x, p2.y)
-  }
+  // noErase();
+  // noFill();
+  // for (let i =0; i<depot.layout.shape[0].length-1; i++){
+  //   p1 = depot.layout.shape[0].seq[i];
+  //   p2 = depot.layout.shape[0].seq[i+1];
+  //   line(p1.x, p1.y, p2.x, p2.y)
+  // }
   fill(40);
-  for (let j=7; j<depot.layout.shape.length; j++){
+  for (let j=depot.ground.length+6; j<depot.layout.shape.length; j++){    // so 6 tam gan cung
     beginShape()
     for (let i =0; i<depot.layout.shape[j].length; i++){
       p1 = depot.layout.shape[j].seq[i];
       vertex(p1.x, p1.y);
     }
     endShape(CLOSE);
-
   }
-  fill("pink")
-  for (let i=0; i<depot.Area.length; i++){
-    translate(depot.Area[i].x_coor,depot.Area[i].y_coor)
-    circle(0,0,20)
-    x_flip = -1 + 2*depot.Area[i].x_flip;
-    for (let j=0; j<depot.Area[i].num_of_bay; j++){
-      for (let k=0; k<depot.Area[i].num_of_row; k++){
-        push();
-        rotate(-depot.Area[i].angle)
-        translate(-j*(depot.contLength+depot.contGap),k*depot.contWidth*x_flip)
-        rect(0,0,-depot.contLength, depot.contWidth*x_flip)
-        pop()
-      }
-    }
-    translate(-depot.Area[i].x_coor,-depot.Area[i].y_coor)
-  }
+  // fill("pink")
+  // for (let i=0; i<depot.Area.length; i++){
+  //   translate(depot.Area[i].x_coor,depot.Area[i].y_coor)
+  //   circle(0,0,20)
+  //   x_flip = -1 + 2*depot.Area[i].x_flip;
+  //   for (let j=0; j<depot.Area[i].num_of_bay; j++){
+  //     for (let k=0; k<depot.Area[i].num_of_row; k++){
+  //       push();
+  //       rotate(-depot.Area[i].angle)
+  //       translate(-j*(depot.contLength+depot.contGap),k*depot.contWidth*x_flip)
+  //       rect(0,0,-depot.contLength, depot.contWidth*x_flip)
+  //       pop()
+  //     }
+  //   }
+  //   translate(-depot.Area[i].x_coor,-depot.Area[i].y_coor)
+  // }
   // fill(0);
   // for (let i=0; i<depot.layout.text.length; i++){
   //   push();
@@ -83,7 +88,7 @@ function drawDepot(){
 }
 
 function preload(){
-  $.getJSON("./data/etd.json", function(data){
+  $.getJSON("./data/etdv1.json", function(data){
     depot = data;
     depot.Area = [];
     init()
@@ -112,13 +117,17 @@ function draw(){
   if (mode=="add_area"){
     circle(mouseX,mouseY,10)
   }
-  scale(0.35);
+  push();
+  scale(scaleFactor);
   rotate(-PI/2);
   translate(-height,width);
   fill(255,0,0);
   circle(0,0,50);
+  line(0,0,20,0);
+  line(0,0,0,40);
   noFill();
   drawDepot();
+  pop();
   if ((mode=="add_area")){
     drawGrid();
   }
@@ -126,18 +135,29 @@ function draw(){
 }
 
 function drawGrid(){
-  // for 
+  push();
+  scale(scaleFactor);
+  for (let i=0; i<windowWidth/scaleFactor; i+=depot.contWidth){
+    line(i,0,i,height/scaleFactor)
+  }
+  for (let i=0; i<windowHeight/scaleFactor; i+=depot.contLength){
+    line(0,i,width/scaleFactor,i)
+  }
+  pop();
 }
 
-// function mousePressed(){
-//   let x = Math.floor(mouseX);
-//   let y = Math.floor(mouseY);
-//   if (mode=="add_area"){
-//     console.log(x,y);
-//     newAreaStart = new Point(mouseX, mouseY);
-//     console.log('newAreaStart: ', newAreaStart);
-//   }
-// }
+function mousePressed(){
+  let x = Math.floor(mouseX);
+  let y = Math.floor(mouseY);
+  // if (mode=="add_area"){
+  //   console.log(x,y);
+  //   newAreaStart = new Point(mouseX, mouseY);
+  //   console.log('newAreaStart: ', newAreaStart);
+  // }
+  let mm = mouseMap(x,y);
+  console.log(mm.x,mm.y);
+  console.log(checkGround(mm.x,mm.y));
+}
 
 // function mouseReleased(){
 //   if (mode=="add_area"){
@@ -172,3 +192,60 @@ function doneAddArea(){
 // function drawNewArea(){
 //   rect(newAreaStart.x, newAreaStart.y, mouseX-newAreaStart.x, mouseY-newAreaStart.y)
 // }
+
+function checkGround(x,y){
+  for (let i=0; i<depot.ground.length; i++){
+    // count = 0;
+    // for (let j=0; j<depot.layout.shape[depot.ground[i].shapeID].length-1; j++){
+    //   let p1 = depot.layout.shape[depot.ground[i].shapeID].seq[j];
+    //   let p2 = depot.layout.shape[depot.ground[i].shapeID].seq[j+1];
+    //   let a = createVector(x-p1.x, y-p1.y);
+    //   let b = createVector(x-p2.x, y-p2.y);
+    //   let c = p5.Vector.mult(a,b);
+    //   if (c.mag()<0){
+    //     count++;
+    //   }
+    // }
+    // if (count==depot.layout.shape[depot.ground[i].shapeID].length-1){
+    //   return i
+    // }
+    if (pointIsInPoly(new Point(x,y), depot.layout.shape[depot.ground[i].shapeID].seq)==1){
+      return i;
+    }
+  }
+  return -1;
+}
+
+function mouseMap(x,y){
+  let p = createVector(x*scaleFactor,y*scaleFactor);
+  p.rotate(-PI/2);
+  p.add(-height,width);
+  return p;
+}
+
+function pointIsInPoly(p, polygon) {
+  var isInside = false;
+  var minX = polygon[0].x, maxX = polygon[0].x;
+  var minY = polygon[0].y, maxY = polygon[0].y;
+  for (var n = 1; n < polygon.length; n++) {
+      var q = polygon[n];
+      minX = Math.min(q.x, minX);
+      maxX = Math.max(q.x, maxX);
+      minY = Math.min(q.y, minY);
+      maxY = Math.max(q.y, maxY);
+  }
+
+  if (p.x < minX || p.x > maxX || p.y < minY || p.y > maxY) {
+      return false;
+  }
+
+  var i = 0, j = polygon.length - 1;
+  for (i, j; i < polygon.length; j = i++) {
+      if ( (polygon[i].y > p.y) != (polygon[j].y > p.y) &&
+              p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x ) {
+          isInside = !isInside;
+      }
+  }
+
+  return isInside;
+}

3d00bd222277da7e934d3b950ce8ecdacbdeef13
Loc Huynh <quanglochuynh@gmail.com>
2022-10-31 15:13:47 +0700

    .

--- yardplan_creator/sketch.js
+++ yardplan_creator/sketch.js
@@ -1,9 +1,33 @@
 const largeFontSize = 28
+let depot;
+let mode="view";
+let newAreaStart=undefined;
+let newAreaEnd=undefined;
 
+class Point{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+class Area{
+  constructor(x,y,w,h){
+    this.name = undefined;
+    this.data = undefined;
+    this.x_coor = x;
+    this.y_coor = y;
+    this.angle = undefined;
+    this.x_flip = undefined;
+    this.y_flip = undefined;
+    this.width = undefined;
+    this.height = undefined;
+    this.num_of_bay = w;
+    this.num_of_row = h;
+  }
+}
 
 function drawDepot(){
   push();
-  rotate(-PI/2)
   translate(-height/2, width/2);
   strokeWeight(2)
   beginShape();
@@ -13,17 +37,18 @@ function drawDepot(){
   }
   endShape(CLOSE);
   noFill();
-  for (let j=1; j<depot.layout.shape.length; j++){
+  for (let i =0; i<depot.layout.shape[0].length-1; i++){
+    p1 = depot.layout.shape[0].seq[i]
+    p2 = depot.layout.shape[0].seq[i+1]
+    line(p1.x, p1.y, p2.x, p2.y)
+  }
+  for (let j=7; j<depot.layout.shape.length; j++){
     for (let i =0; i<depot.layout.shape[j].length-1; i++){
       p1 = depot.layout.shape[j].seq[i]
       p2 = depot.layout.shape[j].seq[i+1]
       line(p1.x, p1.y, p2.x, p2.y)
     }
   }
-  // translate(400,-600)
-  // rotate(-PI/2)
-  // translate(-width/2, height/2);
-
   fill("pink")
   for (let i=0; i<depot.Area.length; i++){
     translate(depot.Area[i].x_coor,depot.Area[i].y_coor)
@@ -37,21 +62,19 @@ function drawDepot(){
         rect(0,0,-depot.contLength, depot.contWidth*x_flip)
         pop()
       }
-      // translate(-depot.contGap,0)
     }
     translate(-depot.Area[i].x_coor,-depot.Area[i].y_coor)
-
   }
-  fill(0);
-  for (let i=0; i<depot.layout.text.length; i++){
-    push();
-    textSize(largeFontSize);
-    pos = depot.layout.text[i].position;
-    translate(pos.x, pos.y,11);
-    rotate(Math.PI/2);
-    text(depot.layout.text[i].content, 0,0);
-    pop();
-  }
+  // fill(0);
+  // for (let i=0; i<depot.layout.text.length; i++){
+  //   push();
+  //   textSize(largeFontSize);
+  //   pos = depot.layout.text[i].position;
+  //   translate(pos.x, pos.y,11);
+  //   rotate(Math.PI/2);
+  //   text(depot.layout.text[i].content, 0,0);
+  //   pop();
+  // }
   pop();
 
 }
@@ -59,42 +82,84 @@ function drawDepot(){
 function preload(){
   $.getJSON("./data/etd.json", function(data){
     depot = data;
+    depot.Area = [];
     init()
+    // loop();
+    console.log("Done");
+
   })
-  console.log("Done");
 }
 
 function init(){
-  background(240)
-  drawDepot();
+  // background(240)
+  // drawDepot();
+  // newArea = new Area(0,0)
+  newAreaStart = new Point(0,0);
 }
 
 function setup() {
-  createCanvas(windowWidth-10, windowHeight-20);
+  myCanvas = createCanvas(windowWidth-40, windowHeight-80);
+  myCanvas.parent("main_canvas");
   // debugMode()
   // noLoop();
 }
 
 function draw(){
-  // orbitControl();
-  translate(width/2-200, height/2)
-  scale(0.4)
-  background(240)
-  fill(255,0,0)
-  circle(0,0,50)
-  fill(255)
+  background(240);
+  if (mode=="add_area"){
+    circle(mouseX,mouseY,10)
+  }
+  scale(0.35);
+  rotate(-PI/2);
+  translate(-height,width);
+  fill(255,0,0);
+  circle(0,0,50);
+  noFill();
   drawDepot();
-  noLoop();
+  if ((mode=="add_area")&&(newAreaStart!=undefined)){
+    drawNewArea();
+  }
+  // noLoop();
 }
 
 function mousePressed(){
-  x = mouseX;
-  y = mouseY;
-  console.log(x,y);
-  for (let i=0; i<depot.Area.length; i++){
-    shapeID = depot.Area[i].shapeID;
-    for (let j=0; j<depot.layout.shape[shapeID].length; j++){
-      console.log(depot.layout.shape[shapeID].seq[j].x);
-    }
+  let x = Math.floor(mouseX);
+  let y = Math.floor(mouseY);
+  if (mode=="add_area"){
+    console.log(x,y);
+    newAreaStart = new Point(mouseX, mouseY);
+    console.log('newAreaStart: ', newAreaStart);
   }
+}
+
+function mouseReleased(){
+  if (mode=="add_area"){
+    // console.log(x,y);
+    newAreaEnd = new Point(Math.floor(mouseX-newAreaEnd.x), Math.floor(mouseY-newAreaEnd.y));
+    console.log('newAreaEnd: ', newAreaEnd);
+    mode = "view";
+  }
+}
+
+// function mouseDragged(){
+//   if (mode=="add_area"){
+//     drawNewArea();
+//   }
+// }
+
+function addArea(){
+  mode = "add_area";
+  console.log('mode: ', mode);
+}
+
+function doneAddArea(){
+  mode = "view";
+  console.log('mode: ', mode);
+  depot.Area.push(new Area(newAreaStart.x, newAreaStart.y, newAreaEnd.x, newAreaEnd.y));
+  console.log('newArea: ', newArea);
+
+}
+
+function drawNewArea(){
+  rect(newAreaStart.x, newAreaStart.y, mouseX-newAreaStart.x, mouseY-newAreaStart.y)
 }

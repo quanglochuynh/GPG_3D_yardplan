8cfd7dfb2f681f12e2c0c86dd035e6b4bb6fa2be
Loc Huynh <quanglochuynh@gmail.com>
2022-10-31 11:52:18 +0700

    .

--- yardplan_creator/sketch.js
+++ yardplan_creator/sketch.js
@@ -1,9 +1,10 @@
-const depot_scale = 3
+const largeFontSize = 28
+
 
 function drawDepot(){
   push();
-  translate(width/2, height/2);
   rotate(-PI/2)
+  translate(-height/2, width/2);
   strokeWeight(2)
   beginShape();
   for (let j=1; j<depot.layout.shape[0].length; j++){
@@ -20,24 +21,39 @@ function drawDepot(){
     }
   }
   // translate(400,-600)
-  pop();
+  // rotate(-PI/2)
+  // translate(-width/2, height/2);
 
-  fill("red")
-  for (let i=0; i<3; i++){
-    translate(depot.Area[i].y_coor,-depot.Area[i].x_coor)
+  fill("pink")
+  for (let i=0; i<depot.Area.length; i++){
+    translate(depot.Area[i].x_coor,depot.Area[i].y_coor)
     circle(0,0,20)
-
-    // for (let j=0; j<depot.Area[i].num_of_bay; j++){
-    //   for (let k=0; k<depot.Area[i].num_of_row; k++){
-    //     push();
-    //     translate(k*10,j*20)
-    //     circle(0,0,4)
-    //     pop()
-    //   }
-    // }
-    translate(-depot.Area[i].y_coor,+depot.Area[i].x_coor)
+    x_flip = -1 + 2*depot.Area[i].x_flip;
+    for (let j=0; j<depot.Area[i].num_of_bay; j++){
+      for (let k=0; k<depot.Area[i].num_of_row; k++){
+        push();
+        rotate(-depot.Area[i].angle)
+        translate(-j*(depot.contLength+depot.contGap),k*depot.contWidth*x_flip)
+        rect(0,0,-depot.contLength, depot.contWidth*x_flip)
+        pop()
+      }
+      // translate(-depot.contGap,0)
+    }
+    translate(-depot.Area[i].x_coor,-depot.Area[i].y_coor)
 
   }
+  fill(0);
+  for (let i=0; i<depot.layout.text.length; i++){
+    push();
+    textSize(largeFontSize);
+    pos = depot.layout.text[i].position;
+    translate(pos.x, pos.y,11);
+    rotate(Math.PI/2);
+    text(depot.layout.text[i].content, 0,0);
+    pop();
+  }
+  pop();
+
 }
 
 function preload(){
@@ -61,12 +77,24 @@ function setup() {
 
 function draw(){
   // orbitControl();
-  translate(width/2, height/2)
-  scale(0.2)
+  translate(width/2-200, height/2)
+  scale(0.4)
   background(240)
   fill(255,0,0)
   circle(0,0,50)
   fill(255)
   drawDepot();
+  noLoop();
+}
 
+function mousePressed(){
+  x = mouseX;
+  y = mouseY;
+  console.log(x,y);
+  for (let i=0; i<depot.Area.length; i++){
+    shapeID = depot.Area[i].shapeID;
+    for (let j=0; j<depot.layout.shape[shapeID].length; j++){
+      console.log(depot.layout.shape[shapeID].seq[j].x);
+    }
+  }
 }
